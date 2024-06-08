0257cb41c970592517ff922470d1eff750fb592f
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-06-08 23:05:10 +0300

    update file with enable cache to increase the process time

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -5,7 +5,7 @@ from steps.evaluation import evaluate_model
 from steps.ingest_data import ingest_data
 from steps.model_train import train_model
 
-@pipeline
+@pipeline(enable_cache=True)
 def train_pipeline(data_path: str):
     df = ingest_data()
     clean_df(df)
