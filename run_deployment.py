9f96a4b479f8afac7c8d59244bb1612aa936d6c8
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-06-14 14:25:54 +0300

    make run for main calling of deployment_pipeline file

---
+++ run_deployment.py
@@ -0,0 +1,15 @@
+from pipelines import deployment_pipeline, training_pipeline
+import click 
+
+DEPLOY = "deploy"
+PREDICT = "predict"
+DEPLOY_AND_PREDICT = "deploy_and_predict"
+
+@click.command()
+@click.option(
+    "--config",
+    "-c",
+    type=click.Choice([DEPLOY, PREDICT, DEPLOY_AND_PREDICT]),
+    default=DEPLOY_AND_PREDICT,
+    help="Optionally you can choose to only run the deployment "
+    )
